e57d53a063557a7ca784c101349ef11234c389fc
Aaron <fong_lun@live.com>
2021-11-25 20:48:01 -0500

    1

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,45 @@ import Robot from './components/Robot';
 import ShoppingCart from './components/ShoppingCart';
 
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={robot} className={styles.appLogo} alt='logo' />
-        <h1>Rock 'Em Sock 'Em Robots.</h1>
+interface Props { }
+
+interface State {
+  robotGallery: any[]
+  count: number
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      robotGallery: [],
+      count: 0
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => this.setState({ robotGallery: data }))
+  }
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={robot} className={styles.appLogo} alt='logo' />
+          <h1>Rock 'Em Sock 'Em Robots.</h1>
+        </div>
+        <button onClick={() => {
+          this.setState({ count: this.state.count + 1 })
+        }}>Click</button>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map(item => <Robot id={item.id} email={item.email} name={item.name} key={item.id} />)}
+        </div>
       </div>
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {robots.map(item => <Robot id={item.id} email={item.email} name={item.name} />)}
-      </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
