130bd3cf33c77ecbfd106ab5d2af17ac17229104
Aaron <fong_lun@live.com>
2021-11-28 02:46:24 -0500

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,31 @@ interface State {
 const App: React.FC = () => {
   const [count, setCount] = useState<number>(0)
   const [robotGallery, setrobotGallery] = useState<any>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>()
 
   useEffect(() => {
     document.title = `clicked ${count}`
   }, [count])
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(data => setrobotGallery(data))
+    const fetchData = async () => {
+      setLoading(true)
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        // .then(res => res.json())
+        // .then(data => setrobotGallery(data))
+        const data = await response.json()
+        setrobotGallery(data)
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message)
+
+        }
+      }
+      setLoading(false)
+    }
+    fetchData()
   }, [])
   // componentDidMount() {
   //   fetch("https://jsonplaceholder.typicode.com/users")
@@ -42,9 +58,13 @@ const App: React.FC = () => {
       }}>Click</button>
       <span>count: {count}</span>
       <ShoppingCart />
-      <div className={styles.robotList}>
-        {robotGallery.map(item => <Robot id={item.id} email={item.email} name={item.name} key={item.id} />)}
-      </div>
+      {(!error || error !== "") && <div>webError:{error}</div>}
+      {!loading ?
+        <div className={styles.robotList}>
+          {robotGallery.map(item => <Robot id={item.id} email={item.email} name={item.name} key={item.id} />)}
+        </div>
+        : <h2>Loading data ...</h2>
+      }
     </div>
   );
 }
